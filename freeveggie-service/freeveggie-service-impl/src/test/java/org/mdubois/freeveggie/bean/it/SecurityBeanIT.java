6774158b435ae8ad4dd467d255404c9f233c3c00
Mickael Dubois <baloo2401@users.noreply.github.com>
2014-09-06 16:14:47 +0000

    fix it test

--- freeveggie-service/freeveggie-service-impl/src/test/java/org/mdubois/freeveggie/bean/it/SecurityBeanIT.java
+++ freeveggie-service/freeveggie-service-impl/src/test/java/org/mdubois/freeveggie/bean/it/SecurityBeanIT.java
@@ -170,9 +170,9 @@ public class SecurityBeanIT extends AbstractBeanIntegrationTest {
         //Set the user test as a validate status
         DataSource freeveggieDatasource  = (DataSource) container.getContext().lookup("jdbc/freeveggie");
         String sql = "UPDATE t_user SET usr_temp_password = '' WHERE usr_id = 5";
-        try (Statement stmt = freeveggieDatasource.getConnection().createStatement()) {
-            stmt.executeUpdate(sql);
-        }
+        Statement stmt = freeveggieDatasource.getConnection().createStatement();
+        stmt.executeUpdate(sql);
+        stmt.close();
 
         final String pEmail = "deletetest@gmail.com";
         securityBean.generateTempPassword(uuid);
